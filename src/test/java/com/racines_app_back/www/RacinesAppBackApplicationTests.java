251f378e39b500aa5e87b5810e5e46f676f80264
Amadou Landoure <amadoulandoure004@gmail.com>
2026-01-11 15:28:00 +0000

    first commit

---
+++ src/test/java/com/racines_app_back/www/RacinesAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.racines_app_back.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacinesAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
